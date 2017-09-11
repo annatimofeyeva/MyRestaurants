5029f06969d783c4411534ae7574ee29387e7d83
Anna Timofeeva and Michael Dunlap <anna+michael@>
2017-09-11 10:24:30 -0700

    pulls data from Yelp API

---
+++ app/src/main/java/com/epicodus/annatimofeyeva/myrestaurants/Constants.java
@@ -0,0 +1,14 @@
+package com.epicodus.annatimofeyeva.myrestaurants;
+
+/**
+ * Created by Guest on 9/11/17.
+ */
+
+public class Constants {
+    public static final String YELP_ACCESS_TOKEN = BuildConfig.YELP_ACCESS_TOKEN;
+    public static final String YELP_ACCESS_TYPE = BuildConfig.YELP_ACCESS_TYPE;
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search?term=food";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+
+
+}
