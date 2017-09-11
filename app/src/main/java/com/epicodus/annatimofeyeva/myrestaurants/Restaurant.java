fdf54686f5560ac5916be4602e6b3c025fb64df5
Anna Timofeeva and Michael Dunlap <anna+michael@>
2017-09-11 15:13:14 -0700

    app will show a dynamic list of restaurants according to location, logCat shows additional properties

---
+++ app/src/main/java/com/epicodus/annatimofeyeva/myrestaurants/Restaurant.java
@@ -0,0 +1,81 @@
+package com.epicodus.annatimofeyeva.myrestaurants;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Guest on 9/11/17.
+ */
+
+public class Restaurant {
+    private String mName;
+    private String mPhone;
+    private String mWebsite;
+    private double mRating;
+    private String mImageUrl;
+//    private ArrayList<String> mAddress = new ArrayList<>();
+    private double mLatitude;
+    private double mLongitude;
+//    private ArrayList<String> mCategories = new ArrayList<>();
+
+//    public Restaurant(String name, String phone, String website,
+//                      double rating, String imageUrl, ArrayList<String> address,
+//                      double latitude, double longitude, ArrayList<String> categories) {
+//        this.mName = name;
+//        this.mPhone = phone;
+//        this.mWebsite = website;
+//        this.mRating = rating;
+//        this.mImageUrl = imageUrl;
+//        this.mAddress = address;
+//        this.mLatitude = latitude;
+//        this.mLongitude = longitude;
+//        this.mCategories = categories;
+//    }
+
+    public Restaurant(String name, String phone, String website, double rating, String imageUrl, double latitude, double longitude) {
+        this.mName = name;
+        this.mPhone = phone;
+        this.mWebsite = website;
+        this.mRating = rating;
+        this.mImageUrl = imageUrl;
+        this.mLongitude = longitude;
+        this.mLatitude = latitude;
+//        this.mAddress = address;
+//        this.mCategories = categories;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+    public String getWebsite() {
+        return  mWebsite;
+    }
+
+    public double getRating() {
+        return mRating;
+    }
+
+    public String getImageUrl(){
+        return mImageUrl;
+    }
+
+//    public ArrayList<String> getAddress() {
+//        return mAddress;
+//    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+//
+//    public ArrayList<String> getCategories() {
+//        return mCategories;
+//    }
+}
